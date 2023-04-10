0e6b79e8cffa6ba860c937e6aaf3a74ea5d7bb78
raj patel <rajumaliba4302@gmail.com>
2023-04-10 11:27:23 +0530

    commit code

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,24 +1,4 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './Navbar.css'
 
-// function Navbar() {
-//   return (
-//     <nav className="navbar">
-//         <a className="logo">News App</a>
-//         <ul className="menu">
-//           <li><Link to="/">Home</Link></li>
-//           <li><Link to="/world">World</Link></li>
-//           <li><Link to="/technology">Technology</Link></li>
-//           <li><Link to="/business">Business</Link></li>
-//           <li><Link to= "/sports">Sports</Link></li>
-//           <li><Link to="/entertainment">Entertainment</Link></li>
-//         </ul>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
 
 
 import React from 'react';
